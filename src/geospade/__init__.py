f4db546a001c721bf709a30a1b882f0992154364
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2024-10-24 14:10:39 +0200

    r remove pyscaffold and deprecated pkg_resources

--- src/geospade/__init__.py
+++ src/geospade/__init__.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = 'unknown'
-finally:
-    del get_distribution, DistributionNotFound
+__version__ = "v0.2.4"
 
 # declare global variables
 DECIMALS = 9  # TODO: how should this be declared?
